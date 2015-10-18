5715ea867f7a69aa733e31e16c8621938ff43b80
perlancar (@pc-home) <perlancar@gmail.com>
2015-10-18 08:23:30 +0700

    Initial commit (unfinished)

---
+++ lib/App/mcpan.pm
@@ -0,0 +1,17 @@
+package App::mcpan;
+
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: A CLI for WWW::MetaCPAN::Simple
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<WWW::MetaCPAN::Simple>
+
+L<mcpan>
---
+++ lib/App/mcpan/Packed.pm
@@ -0,0 +1,10 @@
+package App::mcpan::Packed;
+
+# DATE
+# VERSION
+
+# PACKED_MODULES
+# PACKED_DISTS
+
+1;
+# ABSTRACT: A simple CLI for WWW::MetaCPAN::Simple (just an empty module for metadata)
